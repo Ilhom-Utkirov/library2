27763acf1d5d7a4a9ee5243cf18f542c0dd1b269
Ilhom Utkirov <Inspiration95@mail.ru>
2021-07-27 20:33:37 +0500

    now it is working in login check if all is alowed

--- src/main/java/com/example/v2/basicSecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/v2/basicSecurity/security/ApplicationSecurityConfig.java
@@ -11,14 +11,20 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import static com.example.v2.basicSecurity.security.ApplicationUserRole.STUDENT;
@@ -43,7 +49,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/", "index", "/css/*", "/js/*", "/static/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
                 .anyRequest()
                 .authenticated()
@@ -59,19 +65,21 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
                 .key("somethingverysecured")
                 .rememberMeParameter("remember-me")
-                //.userDetailsService(applicationUserService)
+                .userDetailsService(applicationUserService)
                 .and()
                 .logout()
-                .logoutUrl("/logout")
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET")) // https://docs.spring.io/spring-security/site/docs/4.2.12.RELEASE/apidocs/org/springframework/security/config/annotation/web/configurers/LogoutConfigurer.html
-                .clearAuthentication(true)
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID", "remember-me")
-                .logoutSuccessUrl("/login")
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"/*, "GET"*/)) // https://docs.spring.io/spring-security/site/docs/4.2.12.RELEASE/apidocs/org/springframework/security/config/annotation/web/configurers/LogoutConfigurer.html
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID", "remember-me")
+                    .logoutSuccessUrl("/login")
+                .permitAll()
                 .and()
         .exceptionHandling()
         .accessDeniedPage("/errors/403");
-
+//         .and()
+//                .exceptionHandling().authenticationEntryPoint(new AjaxAwareAuthenticationEntryPoint("/login"));
     }
 
     @Override
@@ -90,6 +98,28 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 //        return new BCryptPasswordEncoder();
 //    }
 
+
+//
+//public class AjaxAwareAuthenticationEntryPoint extends LoginUrlAuthenticationEntryPoint
+//{
+//    public AjaxAwareAuthenticationEntryPoint(String loginUrl) {
+//        super(loginUrl);
+//    }
+//
+//    @Override
+//    public void commence(
+//            HttpServletRequest request,
+//            HttpServletResponse response,
+//            AuthenticationException authException)
+//            throws IOException, ServletException {
+//        String ajaxHeader = ((HttpServletRequest) request).getHeader("X-Requested-With");
+//        boolean isAjax = "XMLHttpRequest".equals(ajaxHeader);
+//        if (isAjax) {
+//            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Ajax REquest Denied (Session Expired)");
+//        } else {
+//            super.commence(request, response, authException);
+//        }
+//    }
 
     @Bean
     public DaoAuthenticationProvider daoAuthenticationProvider() {
